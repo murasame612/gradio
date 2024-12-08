6632f70df123110c0ef2c74fe47aa60ad49228f2
HongWen <1084199458@qq.com>
2024-12-08 16:47:43 +0800

    增加注释

--- static_photo.py
+++ static_photo.py
@@ -3,31 +3,55 @@ import json
 from flask import Flask, send_from_directory, send_file, request, jsonify
 import os
 
+"""
+使用flask解决gradio无法实现的功能
+"""
 app = Flask(__name__)
 
 @app.route('/<path:user>/image/<path:filename>')
 def serve_image(user,filename):
+    """
+    静态托管图片
+    :param user:
+    :param filename:
+    :return:
+    """
     absolute_path = os.path.join(f'D:\pythonproject\gradio\\user\\{user}\\latest\\image', filename)
     return send_file(absolute_path)
 
 @app.route('/<path:user>/json/<path:filename>')
 def serve_json(user,filename):
+    """
+    静态托管json文件
+    :param user:
+    :param filename:
+    :return:
+    """
     absolute_path = os.path.join(f'D:\pythonproject\gradio\\user\\{user}\\latest\\json', filename)
     return send_file(absolute_path)
 
 @app.route('/call_python_function', methods=['POST'])
 def call_python_function():
+    """
+    点击按钮后的处理函数,按钮功能是将记录改成正确的,即将json文件中的correct字段改为True
+    :return:
+    """
+    #按钮返回的json数据
     data = request.get_json()
+    #获得图片名
     image_name = data.get('image_name')
+    #获得用户名
     user = data.get("user")
-    # 根据传递的图片名称执行相应的 Python 函数
-    # img_path = os.path.join("./user", user, "latest", "image",image_name)
-    # os.remove(img_path)
+    #获得图片的json文件名
     j_name = image_name.replace(".png", ".json")
+    #根据user图片的json文件路径
     json_file_path = os.path.join("./user", user, "latest", "json",j_name)
+    #打开json文件
     with open(json_file_path, 'r', encoding='GBK') as f:
         data = json.load(f)
+    #将json文件中的correct字段改为True
     data["correct"] = True
+    #写回json
     with open(json_file_path, 'w') as f:
         json.dump(data, f, indent=4)
 
@@ -37,22 +61,22 @@ def call_python_function():
 
 @app.route('/<user>')
 def show_images(user):
+    """
+    生成用户的HTML页面
+    :param user:
+    :return:
+    """
     html_path = os.path.join("./user",user,"images_gallery.html")
     with open(html_path, 'r', encoding='utf-8') as file:
         html_content = file.read()
     return html_content
 
-@app.route('/call_bottom_function', methods=['POST'])
-def call_bottom_function():
-    data = request.get_json()
-    user = data.get("user")
-    print(f"底部按钮被点击，当前用户：{user}")
-
-    # 在这里执行具体的功能
-    return jsonify({"status": "success", "user": user})
-
 if __name__ == '__main__':
     app.run(debug=True)
 
 def flask_main():
+    """
+    启动flask函数，方便使用线程
+    :return:
+    """
     app.run(port=5000)
